06edbd9c69e8d4fde639ca768b73a79243530a6b
sachinduKavi <cst21017@std.uwu.ac.lk>
2024-04-08 23:31:05 +0530

    fix

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = 3000
 app.use(
     cors({
         origin:'http://localhost:5173',
-        // origin: 'https://openledgers.netlify.app'
+        origin: 'https://openledgers.netlify.app'
     })
 )
 
